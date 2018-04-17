99c7edc69074a3fd071a938233db28b9999fbb59
WouterSpekkink <wouterspekkink@gmail.com>
2018-04-17 22:21:57 +0100

    Changed version number.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[]) {
   QApplication QSoPrA(argc, argv);
 
-  QString version = QString("0.8.7.1");
+  QString version = QString("0.8.7.2");
   QSoPrA.setApplicationVersion(version);
 
   QSharedPointer<EventSequenceDatabase> esd(new EventSequenceDatabase);
