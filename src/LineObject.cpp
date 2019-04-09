520e4199ffbf7f44e104a55dafbecd02719cf663
WouterSpekkink <wouterspekkink@gmail.com>
2019-04-09 18:03:33 +0100

    Fixed the bounding rect issue for line objects a bit better now.

--- src/LineObject.cpp
+++ src/LineObject.cpp
@@ -23,7 +23,9 @@ LineObject::LineObject(QPointF startPos,
 
 QRectF LineObject::boundingRect() const 
 {
-  return QRectF(_startPos, _endPos).normalized();
+  int margin = 19 + _penWidth;
+  return QRectF(_startPos, _endPos).marginsAdded(QMargins(margin, margin, margin, margin))
+    .normalized();
 }
 
 QPainterPath LineObject::shape() const 
